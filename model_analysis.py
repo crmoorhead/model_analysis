15bf20eb4fb6d25448f11cd4af46225ecc16bbdb
Chris Moorhead <46568054+crmoorhead@users.noreply.github.com>
2020-02-03 14:43:25 +0000

    Added plotting function

--- model_analysis.py
+++ model_analysis.py
@@ -107,3 +107,130 @@ def plot_confusion_matrix(y_true, y_pred, classes,*args,**kwargs):
     fig.tight_layout()
     return ax
 
+# TEST DATA
+
+'''xs1=[0,1,2,3,4,5,6]
+xs2=[0,1,2,3,4,5,6]
+
+number_of_trials=100
+ys1=[[0.5+0.1*j+normalvariate(0,0.05) for j in range(7)] for i in range(number_of_trials)]
+ys2=[[0.2+0.2*j+normalvariate(0,0.07) for j in range(7)] for i in range(number_of_trials)]'''
+
+# FUNCTION TO PLOT MANY RUNS OF A GIVEN EXPERIMENT AND COMPARE ON SAME AXIS
+
+# FUNCTION TO PREPARE DATA
+
+# The input must be a list of lists. First entry must be the x-coordinates of the plot while the second is a list of lists of
+# y-coordinates for each run of the experiment. The output of the function will be a dictionary with keywords "x_coords","labels","mean","max","min" and optionally
+# "median", "Q1" and "Q3" for the lower and upper quartiles respectively and "std" and "2std".
+
+def create_data_dict(data_runs,*args,**kwargs):
+    # check that data is of right form and lengths match
+    if data_runs.__class__!=list or data_runs[0].__class__!=list or data_runs[1].__class__!=list:
+        print("Check format of input")
+        return None
+    number_of_xs=len(data_runs[0])
+    if data_runs[1][0].__class__!=list:
+        number_of_trials=1
+        if number_of_xs!=len(data_runs[1]):
+            print("Data points for x and y are of different length.")
+            return None
+        data_runs[1]=[data_runs[1]]
+    else:
+        number_of_trials=len(data_runs[1])
+        if False in [number_of_xs==len(data_runs[1][i]) for i in range(number_of_trials)]:
+            print("Data points for x and y are of different length for at least one run.")
+            return None
+
+    # Create dictionary
+    data_dict={"x_coords":data_runs[0]}
+    # Calculate statistical properties and add to data_dict
+
+    data_dict["mean"]=[sum([data_runs[1][i][j] for i in range(number_of_trials)])/number_of_trials for j in range(number_of_xs)]
+    data_dict["max"] = [max([data_runs[1][i][j] for i in range(number_of_trials)]) for j in range(number_of_xs)]
+    data_dict["min"] = [min([data_runs[1][i][j] for i in range(number_of_trials)]) for j in range(number_of_xs)]
+
+    # Quartiles
+
+    data_dict["median"] = [np.percentile([data_runs[1][i][j] for i in range(number_of_trials)],50) for j in range(number_of_xs)]
+    data_dict["Q1"] = [np.percentile([data_runs[1][i][j] for i in range(number_of_trials)],25) for j in range(number_of_xs)]
+    data_dict["Q3"] = [np.percentile([data_runs[1][i][j] for i in range(number_of_trials)],75) for j in range(number_of_xs)]
+
+    sds = [np.std([data_runs[1][i][j] for i in range(number_of_trials)]) for j in range(number_of_xs)]
+    data_dict["+1 SD"]=[data_dict["mean"][i]+sds[i] for i in range(number_of_xs)]
+    data_dict["-1 SD"] = [data_dict["mean"][i] - sds[i] for i in range(number_of_xs)]
+    data_dict["+2 SD"] = [data_dict["mean"][i] + 2*sds[i] for i in range(number_of_xs)]
+    data_dict["-2 SD"] = [data_dict["mean"][i] - 2*sds[i] for i in range(number_of_xs)]
+
+    if "exp_name" in kwargs:        # Sets the name of set of experiments used to plot a single line
+        data_dict["name"]=kwargs["exp_name"]
+    else:
+        data_dict["name"]="Experimental results"
+
+    if "labels" in kwargs:
+        data_dict["x_label"]=kwargs["labels"][0]
+        data_dict["y_label"] = kwargs["labels"][1]
+    else:
+        data_dict["x_label"]="Independent variable"
+        data_dict["y_label"] = "Dependent variable"
+
+    return data_dict
+
+
+test_data1=create_data_dict([xs1,ys1],exp_name="Experiment 1")
+test_data2=create_data_dict([xs2,ys2],exp_name="Experiment 2")
+
+# Function to create plot
+
+def multi_model_stats(data_stats,*args,**kwargs):
+    if "alpha" in kwargs:
+        alpha=kwargs["alpha"]
+    else:
+        alpha=0.5
+    if "colors" in kwargs:
+        colors=kwargs["colors"]
+    else:
+        colors=["green","blue","red","yellow","purple","pink","grey"] # CHECK THESE ARE ALL VALID!
+        if len(data_stats)>len(colors):
+            print("You need to define a colour scheme for more than 7 sets of experiments.")
+        i=0
+        for d in data_stats:
+            d["color"]=colors[i]
+            i+=1
+    if "title" in kwargs:
+        chart_title=kwargs["title"]
+    else:
+        chart_title=""
+    fig, ax = plt.subplots(1) # ready workspace
+    for d in data_stats:
+        ax.plot(d["x_coords"], d["mean"], lw=2, label=d["name"], color=d["color"]) # Default line width of 2
+        ax.fill_between(d["x_coords"], d["max"], d["min"], facecolor=d["color"], alpha=alpha)
+    if "percentiles" in args:
+        for d in data_stats:
+            ax.plot(d["x_coords"], d["Q1"], lw=1, ls=":",color=d["color"]) # Default line width of 1
+            ax.plot(d["x_coords"], d["Q3"], lw=1, ls=":",color=d["color"])  # Default line width of 1
+    if "median" in args:
+        for d in data_stats:
+            ax.plot(d["x_coords"], d["median"], lw=1, label=d["name"], color=d["color"]) # Default line width of 1
+    if "std" in args:
+        for d in data_stats:
+            ax.plot(d["x_coords"], d["+1 SD"], lw=1, ls="--",color=d["color"])  # Default line width of 1
+            ax.plot(d["x_coords"], d["-1 SD"], lw=1, ls="--",color=d["color"])  # Default line width of 1
+    if "2std" in args:
+        for d in data_stats:
+            ax.plot(d["x_coords"], d["+2 SD"], lw=1, ls="--", color=d["color"])  # Default line width of 1
+            ax.plot(d["x_coords"], d["-2 SD"], lw=1, ls="--", color=d["color"])  # Default line width of 1
+    ax.set_title(chart_title)
+    if len(data_stats)>1:
+        ax.legend(loc='upper left')
+    ax.set_xlabel(choice(data_stats)["x_label"])
+    ax.set_ylabel(choice(data_stats)["y_label"])
+    if "grid" in args:
+        ax.grid()
+    if "rescale" in args:
+        # Put options for scaling X and Y axes
+        pass
+    plt.show()
+
+# multi_model_stats([test_data1,test_data2],"2std","std",title="Experiment under different settings")
+
